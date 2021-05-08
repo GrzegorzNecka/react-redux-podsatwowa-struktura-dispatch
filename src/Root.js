cca22854abd270b259c59e0653a95417a0be3ea7
GrzegorzNecka <strzypior@gmail.com>
2021-05-08 12:09:12 +0200

    1 file modified

--- src/Root.js
+++ src/Root.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import App from './App';
 
 const initialMovies = {
   listName: 'Favourite',
-  movies: ['Rambo III', 'Hakerzy', 'Matrix']
+  list: ['Rambo III', 'Hakerzy', 'Matrix']
 };
 
 function moviesReducer(state = initialMovies, action) {
   switch (action.type) {
-    case 'ADD':
+    case 'ADD_MOVIE':
       return {
         ...state,
-        movies: [...state.movies, action.movie]
+        list: [...state.list, action.item]
       };
 
-    case 'RESET':
-      return { ...state, movies: [] };
+    case 'RESETD_MOVIES':
+      return { ...state, list: [] };
+    default:
+      return state;
+  }
+}
+
+// -------------------------------------------
+
+const initialActors = {
+  listName: 'Best',
+  list: ['Tom Hardy', 'Julia Roberts', 'Angelina Jolie']
+};
+
+function actorsReducer(state = initialActors, action) {
+  switch (action.type) {
+    case 'ADD_ACTOR':
+      return {
+        ...state,
+        list: [...state.list, action.item]
+      };
+
+    case 'RESET_ACTORS':
+      return { ...state, list: [] };
     default:
       return state;
   }
@@ -26,11 +48,11 @@ const store = createStore(moviesReducer);
 
 //window.store = store;
 
-console.log('store', store.getState());
-store.dispatch({ type: 'RESET' });
-console.log('store', store.getState());
-store.dispatch({ type: 'ADD', movie: 'Titanic' });
-console.log('store', store.getState());
+// console.log('store', store.getState());
+// store.dispatch({ type: 'RESET' });
+// console.log('store', store.getState());
+// store.dispatch({ type: 'ADD', movie: 'Titanic' });
+// console.log('store', store.getState());
 
 const Root = () => {
   return <App />;
