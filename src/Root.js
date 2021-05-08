a534e4cbfd6e0f9e4b2b2b8156dd364b3831da89
GrzegorzNecka <strzypior@gmail.com>
2021-05-08 10:54:50 +0200

    1 file modified

--- src/Root.js
+++ src/Root.js
@@ -3,13 +3,20 @@ import { createStore } from 'redux';
 import App from './App';
 
 const initialMovies = {
+  listName: 'Favourite',
   movies: ['Rambo III', 'Hakerzy', 'Matrix']
 };
 
 function moviesReducer(state = initialMovies, action) {
   switch (action.type) {
+    case 'ADD':
+      return {
+        ...state,
+        movies: [...state.movies, action.movie]
+      };
+
     case 'RESET':
-      return { movies: [] };
+      return { ...state, movies: [] };
     default:
       return state;
   }
@@ -17,7 +24,13 @@ function moviesReducer(state = initialMovies, action) {
 
 const store = createStore(moviesReducer);
 
-window.store = store;
+//window.store = store;
+
+console.log('store', store.getState());
+store.dispatch({ type: 'RESET' });
+console.log('store', store.getState());
+store.dispatch({ type: 'ADD', movie: 'Titanic' });
+console.log('store', store.getState());
 
 const Root = () => {
   return <App />;
