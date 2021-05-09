7a882c3f2a308eadb8bd8d4d9e8a143eb396caa3
GrzegorzNecka <strzypior@gmail.com>
2021-05-09 11:20:21 +0200

    1 file modified

--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import actorsReducer from './app/actors/duck';
-import movieReducer from './app/movies/duck';
+import actorsReducer from './app/actors/duck/reducers';
+import movieReducer from './app/movies/duck/reducers';
 
 const rootReucer = combineReducers({
   actors: actorsReducer,
