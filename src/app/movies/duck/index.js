7e8dff59aaadb81fe717383b0d7dc19629f1b929
GrzegorzNecka <strzypior@gmail.com>
2021-05-09 11:41:10 +0200

    2 files modified

--- src/app/movies/duck/index.js
+++ src/app/movies/duck/index.js
@@ -1,5 +1,5 @@
 import moviesReducer from './reducers';
-export { default as actorTypes } from './types';
-export { default as actorActions } from './actions';
+export { default as movieTypes } from './types';
+export { default as movieActions } from './actions';
 
 export default moviesReducer;
