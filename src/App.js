ce6fc94c7d3117de855f31726aefe29b9d7aa56c
GrzegorzNecka <strzypior@gmail.com>
2021-05-09 11:24:34 +0200

    1 file modified, 8 added, 8 deleted

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import App from './App';
+
 
 const store = createStore(rootReducer, composeWithDevTools());
+
+
 
 const App = () => {
   return <p>a</p>;
